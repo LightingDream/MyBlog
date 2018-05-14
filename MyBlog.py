965193dfc29c8ed396ff6daf24096d195ff742ce
LightingDream <2456700829@qq.com>
2018-05-14 19:02:52 +0800

    requirements.txt

--- MyBlog.py
+++ MyBlog.py
@@ -1,14 +1,14 @@
 from app import create_app, db
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-from app.models import Menu, ArticleType
+from flask_migrate import Migrate
+from app.models import Menu, ArticleType, User, Source
 
 app = create_app()
 db.create_all()
 manager = Manager(app)
 migrate = Migrate(app, db)
-manager.add_command('db', MigrateCommand)
 
+app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = '123456'
 #Global variables
 app.jinja_env.globals['ArticleType'] = ArticleType
@@ -21,34 +21,19 @@ app.jinja_env.globals['Menu'] = Menu
 # app.jinja_env.globals['Comment'] = Comment
 # app.jinja_env.globals['BlogView'] = BlogView
 
+
 @manager.command
 def deploy(deploy_type):
-    from flask_migrate import upgrade
-    #upgrade()
     db.drop_all()
     db.create_all()
-    if deploy_type == 'init':
-        pass
 
-    # You must run `python manage.py deploy(product)` before run `python manage.py deploy(test_data)`
     if deploy_type == 'generate_data':
         # step_1:insert navs
         Menu.init_menus()
         ArticleType.init_articleTypes()
-        # step_2:insert articleTypes
-        # ArticleType.insert_articleTypes()
-        # # step_3:generate random articles
-        # Article.generate_fake(100)
-        # # step_4:generate random comments
-        # Comment.generate_fake(300)
-        # # step_5:generate random replies
-        # Comment.generate_fake_replies(100)
-        # # step_4:generate random comments
-        # Comment.generate_fake(300)
+        User.insert_admin('2456700829@qq.com', 'adrui', 'adjsjZBR1996')
+        Source.insert_source()
 
 
 if __name__ == '__main__':
-    l = Menu.query.all()
-    for i in l:
-        print(i.name)
     manager.run()
